7747e8a232b05133419905d5e44c2dc68edcf599
raymondshum <rshum@csumb.edu>
2021-08-14 16:17:08 -0700

    Search

---
+++ js/8_Search/4_kmpStringSearch.js
@@ -0,0 +1,22 @@
+/**
+ * KMP String Search
+ */
+
+/**
+ * Pattern: abcdabc
+ * prefix (left): a, ab, abc, abcd
+ * suffix (right): c, bc, abc, dabc
+ * 
+ * Idea: Is there any prefix == suffix?
+ * - Yes, abc == abc
+ * 
+ * In other words, is there a component at the start of the pattern that
+ * appears anywhere else in the pattern?
+ * 
+ * LPS table
+ * You're building a table that repeating elements
+ *                        
+ * 0 1 2 3 4 5 6 7 8 9 10 [index] 
+ * a b c d e a b f a b c  [string]
+ * 0 0 0 0 0 1 2 0 1 2 3  [table (index+1)] <- Prob. cause 0 show neg. res.
+ */
--- js/8_Search/readme.md
+++ js/8_Search/readme.md
@@ -1,3 +1,5 @@
 # Search Algorithms
 
 Need to find additional resources on KMP String Search.
+
+Review: https://gist.github.com/blasten/d42bd0d814b7df1addea
