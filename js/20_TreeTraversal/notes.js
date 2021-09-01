ca25d7a4d011d69ad18040671a68a6d7340ed15a
raymondshum <rshum@csumb.edu>
2021-09-01 09:46:17 -0700

    Create notes.js

---
+++ js/20_TreeTraversal/notes.js
@@ -0,0 +1,18 @@
+/**
+ * When to use BFS/DFS?
+ */
+/**
+ * Use DFS is the tree is wide
+ * Space complexity is larger when tree is wide (if using BFS)
+ * Time complexity doesn't change
+ * 
+ * Use BFS for long trees for the same reason
+ */
+
+/**
+ * DFS
+ * 
+ * InOrder
+ * If using on a binary search tree, you will get an in order list
+ * Can replicate the tree
+ */
